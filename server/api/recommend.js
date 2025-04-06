4c99fc6b5aad67e3722fe4b3bbb582c274693263
anubhavkushagra <165156798+anubhavkushagra@users.noreply.github.com>
2025-04-06 14:49:24 +0530

    Update recommend.js

--- server/api/recommend.js
+++ server/api/recommend.js
@@ -1,10 +1,10 @@
-// api/recommend.js
-const { getRecommendations } = require('../controllers/aiController');
+// // api/recommend.js
+// const { getRecommendations } = require('../controllers/aiController');
 
-module.exports = async (req, res) => {
-  if (req.method === 'POST') {
-    await getRecommendations(req, res);
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
-  }
-};
+// module.exports = async (req, res) => {
+//   if (req.method === 'POST') {
+//     await getRecommendations(req, res);
+//   } else {
+//     res.status(405).json({ error: 'Method not allowed' });
+//   }
+// };
