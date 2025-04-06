47f08db332dad6ac23a4dd69512a4aec98b3113b
anubhavkushagra <165156798+anubhavkushagra@users.noreply.github.com>
2025-04-06 14:45:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-const { PORT } = require('./config/dotenvConfig');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.json()); // ✅ Allows Express to parse JSON requests
-
-app.get('/', (req, res) => {
-  console.log('GenAI RAG Backend is Running!');
-});
 
 const aiRoutes = require('./routes/aiRoutes');
 app.use('/api', aiRoutes);
 
-app.listen(PORT, () => {
-  console.log(`🔥 Server running on port ${PORT}`);
+app.get('/', (req, res) => {
+  res.send('GenAI RAG Backend is Running!');
 });
+
+
+module.exports = app;
